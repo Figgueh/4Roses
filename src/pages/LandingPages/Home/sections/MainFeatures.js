d80af77f61e35ffb6167dc5d75ffd27c7fb194d3
Alexander Figueiras <figueiras.alexander@gmail.com>
2025-03-05 01:23:33 -0500

    Initial commit Working on home page. Currently worked on 3 components and just started making modifications to the footer section.

---
+++ src/pages/LandingPages/Home/sections/MainFeatures.js
@@ -0,0 +1,63 @@
+/*
+=========================================================
+* Material Kit 2 React - v2.1.0
+=========================================================
+
+* Product Page: https://www.creative-tim.com/product/material-kit-react
+* Copyright 2023 Creative Tim (https://www.creative-tim.com)
+
+Coded by www.creative-tim.com
+
+ =========================================================
+
+* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+*/
+
+// @mui material components
+import Container from "@mui/material/Container";
+import Grid from "@mui/material/Grid";
+import Divider from "@mui/material/Divider";
+
+// Material Kit 2 React components
+import MKBox from "components/MKBox";
+
+// Material Kit 2 React examples
+import DefaultCounterCard from "examples/Cards/CounterCards/DefaultCounterCard";
+
+function MainFeatures() {
+  return (
+    <MKBox component="section" py={3}>
+      <Container>
+        <Grid container item xs={12} lg={9} sx={{ mx: "auto" }}>
+          <Grid item xs={12} md={4}>
+            <DefaultCounterCard
+              count={5}
+              title="Bed rooms"
+              description="sleeps up to 10 people comfortably"
+            />
+          </Grid>
+          <Grid item xs={12} md={4} display="flex">
+            <Divider orientation="vertical" sx={{ display: { xs: "none", md: "block" }, mx: 0 }} />
+            <DefaultCounterCard
+              count={5}
+              suffix="+"
+              title="Bathrooms"
+              description="Complete with poolside bathroom"
+            />
+            <Divider orientation="vertical" sx={{ display: { xs: "none", md: "block" }, ml: 0 }} />
+          </Grid>
+          <Grid item xs={12} md={4}>
+            <DefaultCounterCard
+              count={80}
+              suffix="+"
+              title="Amenities"
+              description="From a washer and dryer to wifi, it's included."
+            />
+          </Grid>
+        </Grid>
+      </Container>
+    </MKBox>
+  );
+}
+
+export default MainFeatures;
