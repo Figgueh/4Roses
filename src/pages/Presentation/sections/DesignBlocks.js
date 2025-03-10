51fe824cbc05eb3e1ff8dec7c9d63af767237c6d
Alexander Figueiras <figueiras.alexander@gmail.com>
2025-03-10 17:35:36 -0400

    Updated included amenities to show pictures of the item and also modified the code to allow for a sub title text to provide more details.
    Also cleaned the images folder of some pictures that aren't going to be used.

--- src/pages/Presentation/sections/DesignBlocks.js
+++ src/pages/Presentation/sections/DesignBlocks.js
@@ -46,10 +46,10 @@ function DesignBlocks() {
       </Grid>
       <Grid item xs={12} lg={9}>
         <Grid container spacing={3}>
-          {items.map(({ image, name, count, route, pro }) => (
+          {items.map(({ image, name, sub, route, pro }) => (
             <Grid item xs={12} md={4} sx={{ mb: 2 }} key={name}>
               <Link to={pro ? "/" : route}>
-                <ExampleCard image={image} name={name} count={count} pro={pro} />
+                <ExampleCard image={image} name={name} sub={sub} pro={pro} />
               </Link>
             </Grid>
           ))}
