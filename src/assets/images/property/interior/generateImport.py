94839edef3f208236801b87b02a24e14746b0809
Alexander Figueiras <figueiras.alexander@gmail.com>
2025-03-13 01:42:47 -0400

    Python script now generates thumbnails and places them in a folder.
    The lightroom for the photo gallary now displays them

--- src/assets/images/property/interior/generateImport.py
+++ src/assets/images/property/interior/generateImport.py
@@ -14,16 +14,24 @@ for file in files:
     if file != "generateImport.py":
         img = Image.open(dir_path + "\\" + file)
         width,height = img.size
-        imports.append([file, str(round(width / 4)), str(round(height / 4))])
+        imports.append([file, str(width), str(height)])
+
+        # Generate thumbnails and save them in the folder.
+        thumbnailSize = 256
+        img.thumbnail((thumbnailSize,thumbnailSize), Image.LANCZOS)
+        img.save(dir_path + "/" + str(thumbnailSize) + "/" + file, "JPEG")
 
 # Print for the imports.
 for thing in imports:
     print("import " + thing[0][:-4].capitalize() + " from \"" + url + thing[0] + "\";")
+    print("import " + thing[0][:-4].capitalize() + "Thumb" + " from \"" + url + str(thumbnailSize) + "/" + thing[0] + "\";")
 
 
 # Print for the photos array.
 for thing in imports:
-    print("  { src: " + thing[0][:-4].capitalize() + ", width: " + thing[1] +", height: " + thing[2] + " },")
+    srcSetFull = "      { src: " + thing[0][:-4].capitalize() + ", width: " + thing[1] + ", height: " + thing[2] + " },"
+    srcSetTumb = "      { src: " + thing[0][:-4].capitalize() + "Thumb, width: " + str(thumbnailSize) + ", height: " + str(thumbnailSize) + " },"
+    print("  {\n    src: " + thing[0][:-4].capitalize() + ",\n    width: " + str(round(int(thing[1]) /4)) +",\n    height: " +  str(round(int(thing[2]) /4)) + ",\n    " + "srcSet: [\n" + srcSetFull + "\n" + srcSetTumb + "\n    ],\n  },")
 
 
 
