c25660c50c00722c80b958d651d688a0ce9e0ecc
Alexander Figueiras <figueiras.alexander@gmail.com>
2025-10-19 15:28:41 -0400

    fix: ensure Puppeteer launch options are correctly set for improved functionality

--- backend/middlewares/scraper.js
+++ backend/middlewares/scraper.js
@@ -3,6 +3,8 @@ import puppeteer from "puppeteer";
 const scrapePage = async (url) => {
   const browser = await puppeteer.launch({
     headless: true,
+    args: ["--no-sandbox", "--disable-setuid-sandbox"],
+    executablePath: process.env.PUPPETEER_EXECUTABLE_PATH || puppeteer.executablePath(),
   });
   const page = await browser.newPage();
 
