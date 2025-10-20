50fd71e4740a4ac1651e797f92772c0b56596701
Alexander Figueiras <figueiras.alexander@gmail.com>
2025-10-19 21:25:26 -0400

    feat: implement Dockerfiles for production and development with Chromium installation

--- backend/middlewares/scraper.js
+++ backend/middlewares/scraper.js
@@ -4,7 +4,6 @@ const scrapePage = async (url) => {
   const browser = await puppeteer.launch({
     headless: true,
     args: ["--no-sandbox", "--disable-setuid-sandbox"],
-    executablePath: process.env.PUPPETEER_EXECUTABLE_PATH || puppeteer.executablePath(),
   });
   const page = await browser.newPage();
 
