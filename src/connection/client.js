9f1fb805d29faebff7eff9a04566eb8ba75cc135
Alexander Figueiras <figueiras.alexander@gmail.com>
2026-05-13 12:15:43 -0400

    added console log for REACT_APP_SUPABASE_URL to test

--- src/connection/client.js
+++ src/connection/client.js
@@ -1,5 +1,7 @@
 import { createClient } from "@supabase/supabase-js";
 
+console.log("Here", process.env.REACT_APP_SUPABASE_URL);
+
 const supabase = createClient(
   process.env.REACT_APP_SUPABASE_URL,
   process.env.REACT_APP_SUPABASE_PUBLISHABLE_DEFAULT_KEY,
