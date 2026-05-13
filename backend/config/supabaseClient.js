e824dd2e8b61f7fa5feea8761c5d6197da977b9c
Alexander Figueiras <figueiras.alexander@gmail.com>
2026-05-13 12:54:54 -0400

    added console log to debug backend server

--- backend/config/supabaseClient.js
+++ backend/config/supabaseClient.js
@@ -11,6 +11,8 @@ dotenv.config({ path: path.resolve(__dirname, `../.env.${nodeEnv}`) });
 const supabaseUrl = process.env.SUPABASE_URL;
 const supabaseKey = process.env.SUPABASE_SERVICE_ROLE_KEY;
 
+console.log("Here", process.env.SUPABASE_URL);
+
 if (!supabaseUrl || !supabaseKey) {
   throw new Error("Supabase URL or KEY is missing in environment variables");
 }
